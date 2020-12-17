7b5ba90596a19cccac46266c6f565670715484d2
ErmekKarypbekov <ekarypbekov>
2020-12-17 23:18:15 +0600

    Exam task 1

--- Ekzamen OOP/src/com/company/Main.java
+++ Ekzamen OOP/src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Circle crl = new Circle(8);
+        crl.printCircleInfo();
+
+        System.out.println("Circle area :" + crl.getCircleArea());
+
+        int size = 10, count = 0;
+        Circle[] array = new Circle[size];
+        for (int i = 0; i < array.length; i ++){
+            array [i] = new Circle (i);
+            if (array[i].getRadius() % 3 == 0){
+                count ++;
+            }
+        }
+        System.out.println("Кол- во кругов " + count);
     }
 }
